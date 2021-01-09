02a0f7fa7dc6768f3da07f78601d95c73deb9d0d
Brent Rodriguez <brent.s.rodriguez@gmail.com>
2021-01-09 00:59:28 -0600

    Modal Update

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 'use strict'
+
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const closeModalBtn = document.querySelector('.close-modal')
+const openModalBtn = document.querySelectorAll('.show-modal')
+
+for(let i = 0; i < openModalBtn.length; i++){
+  openModalBtn[i].addEventListener('click', () => {
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+  })
+
+  closeModalBtn.addEventListener('click', () => {
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+  })
+}
